c5eb12a591413de0cf0b4125bb4721ed5c5951e5
RaviAnand Mohabir <moravrav@gmail.com>
2024-09-04 09:55:09 +0200

    feat: :sparkles: remove forceResolver from goField directive to showcase working state

--- graph/model/models_gen.go
+++ graph/model/models_gen.go
@@ -10,10 +10,29 @@ type NewTodo struct {
 	UserID string `json:"userId"`
 }
 
+type PrivateTodo struct {
+	ID   string `json:"id"`
+	Text string `json:"text"`
+	Done bool   `json:"done"`
+	User *User  `json:"user"`
+}
+
+func (PrivateTodo) IsTodo() {}
+
 type Project struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 }
+
+type ProjectTodo struct {
+	ID      string   `json:"id"`
+	Text    string   `json:"text"`
+	Done    bool     `json:"done"`
+	User    *User    `json:"user"`
+	Project *Project `json:"project"`
+}
+
+func (ProjectTodo) IsTodo() {}
 
 type Query struct {
 }
